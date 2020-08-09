7657435063f35841c28caf1bc03a0bad19b1116c
Juan Pablo <juancholasso@hotmail.com>
2020-08-09 18:34:21 -0500

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Router, CanActivate } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontendInalambria';
+
+  public isLogged:boolean = false;
+  private localStorageService;
+  email:string = '';
+  password:string = '';
+  user:any;
+
+  constructor(private service:ApiService) {
+    var user = localStorage.getItem("isLogged");
+    if(user == "true"){
+      this.isLogged = true;
+    }
+    else{
+      this.isLogged = false;
+    }
+  }
+
+  public login(){
+    // console.log(this.email)
+    // console.log(this.password)
+    // this.service.makeLogin(this.email, this.password).subscribe(
+    //   (data:any)=>{
+    //     localStorage.setItem("token", data.token);
+    //     localStorage.setItem("isLogged", "true");
+    //     this.isLogged = true;
+    //     this.user = data.user;
+    //   },
+    //   (err)=>{
+    //     console.log(err)
+    //   }
+    // )
+  }
+
+  public logout(){
+    // this.service.makeLogout().subscribe(
+    //   (data:any)=>{
+    //     localStorage.clear();
+    //     this.isLogged = false;
+    //   },
+    //   (err)=>{
+    //     localStorage.clear();
+    //     this.isLogged = false;
+    //   }
+    // )
+  }
+  
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongsComponent } from './songs/songs.component';
+import { FormsModule, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { TemplateComponent } from './template/template.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import { PlaylistItemComponent } from './playlist-item/playlist-item.component';
+import { PlaylistAddmusicComponent } from './playlist-addmusic/playlist-addmusic.component';
+import { SongsAddsongComponent } from './songs-addsong/songs-addsong.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component' 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsComponent,
+    LoginComponent,
+    TemplateComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    PlaylistComponent,
+    PlaylistItemComponent,
+    PlaylistAddmusicComponent,
+    SongsAddsongComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxAudioPlayerModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
